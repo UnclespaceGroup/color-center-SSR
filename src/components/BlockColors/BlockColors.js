b8b363cf31f08c959736bd7ae28b11c0e67d24db
Ivan Dmitrachkov <dmitrachkovivan@gmail.com>
2019-10-02 18:35:44 +0300

    add: BlockColor fix

--- src/components/BlockColors/BlockColors.js
+++ src/components/BlockColors/BlockColors.js
@@ -2,11 +2,19 @@ import React from 'react'
 import css from './BlockColors.module.scss'
 import Layout from '../Layout/Layout'
 
+export const text = <div>
+  <p>Цветовая гамма насчитывает более 50 основных цветов и оттенков.</p>
+  <p>Доступны три основных типа цвета: металлик, глянцевый и матовый.</p>
+  <p>Можно подобрать цвет под уже имеющуюся мебель в вашем доме</p>
+</div>
+
 const BlockColors = () => (
   <Layout className={css.container}>
     <div className={css.wrapper} >
       <div className={css.title}>Широкий выбор цвета</div>
-      <div className={css.text}>ваы</div>
+      <div className={css.text}>
+        {text}
+      </div>
     </div>
     <div >
       <div className={css.img} />
--- src/components/BlockColors/BlockColorsMobile.js
+++ src/components/BlockColors/BlockColorsMobile.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import css from './BlockColorsMobile.module.scss'
 import Layout from '../Layout/LayoutMobile'
+import { text } from './BlockColors'
 
 const BlockColors = () => (
   <Layout className={css.container}>
     <div className={css.img} />
     <div className={css.wrapper} >
       <div className={css.title}>Широкий выбор цвета</div>
-      <div className={css.text}>ваы</div>
+      <div className={css.text}>{text}</div>
     </div>
   </Layout>
 )
